23cde32cccb0f6bbd5266b812d8a12f5e666163e
DimiMikadze <dimimikadze@gmail.com>
2026-05-11 12:16:31 +0400

    s

--- app/chat/chat-ui.tsx
+++ app/chat/chat-ui.tsx
@@ -60,7 +60,7 @@ function ResetIcon() {
       strokeWidth={1.8}
       strokeLinecap='round'
       strokeLinejoin='round'
-      className='h-5 w-5'
+      className='h-4 w-4'
     >
       <path d='M3 12a9 9 0 1 0 3-6.7' />
       <path d='M3 4v5h5' />
